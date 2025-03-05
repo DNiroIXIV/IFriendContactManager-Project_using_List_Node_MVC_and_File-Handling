98fe09835998593199127e68b2dfc708727acbc3
DNiroIXIV <dnirodhaixiv@gmail.com>
2025-03-05 21:32:39 +0530

    commit01

--- ContactsList.java
+++ ContactsList.java
@@ -1,125 +1,92 @@
-public class ContactsList{
-	private Contact[] contactsArray;
-	private int nextIndex;
-	private double loadFactor;
-	private int initSize;
-	
-	public ContactsList(int initSize, double loadFactor){
-		contactsArray = new Contact[initSize];
-		nextIndex = 0;
-		this.initSize = initSize;
-		this.loadFactor = loadFactor;	
-	}
-	
-	//---------- add contact to last of the contactsArray ----------------
-	public void add(Contact contact){
-		if(nextIndex >= contactsArray.length){
-			extendsArray();
-		}
-		contactsArray[nextIndex++] = contact;		
-	}	
-	
-	public boolean add(int index, Contact contact){
-		if(nextIndex >= contactsArray.length){
-			extendsArray();
-		}
-		if(index>=0 && index<=nextIndex){
-			for(int i = nextIndex-1; i >= index; i--){
-				contactsArray[i+1] = contactsArray[i];
-			}
-			contactsArray[index] = contact;
-			nextIndex++;
-			return true;
-		}
-		return false;
-	}
-	
-	private void extendsArray(){
-		Contact[] tempContactsArray = new Contact[(int)(contactsArray.length*(1+loadFactor))];
-		for(int i = 0; i < contactsArray.length; i++){
-			tempContactsArray[i] = contactsArray[i];
-		}
-		contactsArray = tempContactsArray;
-	}
-		
-	//---------- remove the last contact of the contactsArray ----------------
-	public void remove(){
-		if(!isEmpty()){
-			nextIndex--;
-		}
-	}
-	
-	public boolean remove(int index){
-		if(index>=0 && index<nextIndex){
-			for(int i = index; i < nextIndex-1; i++){
-				contactsArray[i] = contactsArray[i+1];
-			}			
-			nextIndex--;
-			return true;
-		}
-		return false;
-	}
-	
-	public boolean remove(Contact contact){
-		int index = indexOf(contact);
-		return remove(index);
-	}
-	
-	public int indexOf(Contact contact){
-		for(int i = 0; i < nextIndex; i++){
-			if(contact.isIdEquals(contactsArray[i])){
-				return i;				
-			}
-		}
-		return -1;
-	}
-	
-	public boolean searchByContactId(Contact contact){
-		return indexOf(contact) != -1;		
-	}
-	
-	public int searchByNameOrContactNo(Contact contact){
-		for(int i = 0; i < nextIndex; i++){
-			if(contact.isNameEquals(contactsArray[i]) || contact.isContactNumberEquals(contactsArray[i])){
-				return i;
-			}			
-		}
-		return -1;
-	}
-	
-	public boolean setContact(int index, Contact contact){
-		if(index>=0 && index<nextIndex){
-			contactsArray[index] = contact;
-			return true;
-		}
-		return false;
-	}
-	
-	public Contact getContact(int index){
-		if(index>=0 && index<nextIndex){
-			return contactsArray[index];
-		}
-		return null;
-	}	
-	
-	public Contact[] getAllContacts(){
-		Contact[] tempContactsArray = new Contact[nextIndex];
-		for(int i = 0; i < nextIndex; i++){
-			tempContactsArray[i] = contactsArray[i];
-		}
-		return tempContactsArray;
-	}	
-	
-	public boolean isEmpty(){
-		return nextIndex<=0;
-	}
-	
-	public int size(){
-		return nextIndex;
+public class ContactsList {
+	private Node firstNode;
+
+	public ContactsList() {
+		firstNode = null;
 	}
 
-	public void clear(){
-		contactsArray = new Contact[initSize];
-		nextIndex = 0;
+	public boolean addFirst(Contact contact) {// add to the first
+		Node node = new Node(contact);
+		node.nextNode = firstNode;
+		firstNode = node;
+		return true;
+	}
+
+	public boolean addLast(Contact contact) {// add to the last/Insertion order
+		if (isEmpty()) {
+			return addFirst(contact);
+		} else {
+			Node node = new Node(contact);
+			Node lastNode = firstNode;
+			while (lastNode.nextNode != null) {
+				lastNode = lastNode.nextNode;
+			}
+			lastNode.nextNode = node;
+		}
+		return true;
+	}
+
+	public boolean add(Contact contact) {// add per insertion order
+		return addLast(contact);
+	}
+
+	public boolean add(int index, Contact contact) {
+		if (index >= 0 && index <= size()) {
+			if (index == 0) {
+				return addFirst(contact);
+			}else{
+				Node node = new Node(contact);
+				int i = 0;
+				Node tempNode = firstNode;
+				while (i < index-1) {
+					tempNode = tempNode.nextNode;
+					i++;
+				}
+				node.nextNode = tempNode.nextNode;
+				tempNode.nextNode = node;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public int indexOf(Contact contact) {
+		int index = 0;
+		Node tempNode = firstNode;
+		while (tempNode != null) {
+			if (contact.isIdEquals(tempNode.contact)) {
+				return index;
+			}
+			tempNode = tempNode.nextNode;
+			index++;
+		}
+		return -1;
+	}
+
+	public boolean search(Contact contact) {
+		return indexOf(contact) != -1;
+	}
+
+	public int size(){
+		int count = 0;
+		Node tempNode = firstNode;
+		while (tempNode != null) {
+			count++;
+			tempNode = tempNode.nextNode;
+		}
+		return count;
+	}
+
+	public boolean isEmpty() {
+		return firstNode == null;
+	}
+
+	public class Node {
+		private Contact contact;
+		private Node nextNode;
+
+		public Node(Contact contact) {
+			this.contact = contact;
+		}
 	}
 }
